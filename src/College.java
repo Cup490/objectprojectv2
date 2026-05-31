80e71289845f3b9388b180a3b1f8b3235b89235c
elad katz <elad8katz@gmail.com>
2026-05-31 09:56:24 +0300

    finished final touches with cases, let me know if all works

--- src/College.java
+++ src/College.java
@@ -194,12 +194,6 @@ public class College {
         departments = newArr;
     }
 
-    private void Informationaboutlecturers(Lecturer name) {
-        for (int i = 0; i < lecturersCount; i++) {
-            if (findLecturerByName(name))
-        }
-    }
-
     // get & set
     public String getUniName() {
         return uniName;
